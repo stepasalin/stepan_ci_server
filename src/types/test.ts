755ed651113af0eb2dbb2fcf2004daa559258213
Stepan Salin <ssalin@artec-group.com>
2021-08-18 15:35:45 +0300

    add run reference to test interface

--- src/types/test.ts
+++ src/types/test.ts
@@ -1,8 +1,10 @@
-import {Document} from 'mongoose';
+import {Document, Types} from 'mongoose';
+import {IRun} from './run';
 
 export interface ITest extends Document {
   name: string
   description: string
   status: string
   runCmd: string
+  runs: Types.Array<IRun>
 }
