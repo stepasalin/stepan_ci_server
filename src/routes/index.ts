8c7de4463ffe48b27d28eacb1e8c140dd63816e2
Stepan Salin <ssalin@artec-group.com>
2021-08-18 15:32:49 +0300

    add new routes for run entity

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import {Router} from 'express';
-// import {getTests, addTest, updateTest, deleteTest} from '../controllers/tests';
 import {getTests, addTest} from '../controllers/tests';
+import {getRuns, addRun} from '../controllers/runs';
 import bodyParser from 'body-parser';
 
 const jsonParser = bodyParser.json();
@@ -8,11 +8,9 @@ const jsonParser = bodyParser.json();
 const router: Router = Router();
 
 router.get('/all-tests', getTests);
-
 router.post('/add-test', jsonParser, addTest);
+router.get('/all-runs', getRuns);
+router.post('/add-run', jsonParser, addRun);
 
-// router.put('/edit-todo/:id', updateTest);
-
-// router.delete('/delete-todo/:id', deleteTest);
 
 export default router;
