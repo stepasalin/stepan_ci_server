a75c05afa4bed9b80ec87cad462cb40c066e1078
Stepan Salin <ssalin@artec-group.com>
2021-08-23 17:41:12 +0300

    progress

--- src/specs/simple_spec.ts
+++ src/specs/simple_spec.ts
@@ -1,5 +1,12 @@
-describe('auth', () => {
-  it('blah blah', async () => {
-    expect(1).toEqual(1);
+import {app} from '../app';
+const request = require('supertest');
+
+describe('GET / - a simple api endpoint', () => {
+  it('Hello API Request', async () => {
+    const result = await request(app).get('/all-runs');
+    expect(result.text).toEqual('hello');
+    expect(result.statusCode).toEqual(200);
   });
 });
+
+
