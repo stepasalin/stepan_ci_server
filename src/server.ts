fdf8eac1dcc6f36b316fa4458b5bed41294b0e99
Stepan Salin <ssalin@artec-group.com>
2021-08-23 20:53:45 +0300

    new connection to db

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,7 @@
 import {app} from './app';
-import mongoose from 'mongoose';
-import {PORT, MONGO_URI}
+import {PORT}
   from './config';
 
-const mongoOptions = {useNewUrlParser: true, useUnifiedTopology: true};
-mongoose.set('useFindAndModify', false);
-
-mongoose
-    .connect(MONGO_URI, mongoOptions)
-    .then(() =>
-      app.listen(PORT, () =>
-        console.log(`Server running on http://localhost:${PORT}`),
-      ),
-    )
-    .catch((error) => {
-      throw error;
-    });
+app.listen(PORT, () => {
+  console.log(`Server runs on 127.0.0.1:${PORT}`);
+});
