364a621f981e0c1338c6d06409570dd67e9e24c3
Stepan Salin <ssalin@artec-group.com>
2021-08-26 15:25:57 +0300

    test somehow works

--- src/specs/simple_spec.ts
+++ src/specs/simple_spec.ts
@@ -1,12 +1,26 @@
 import {app} from '../app';
 import db from '../database/connection';
+import {ITest} from './../types/test';
+import Test from './../models/test';
+import {Schema} from 'mongoose';
 const request = require('supertest');
 
-describe('GET / - a simple api endpoint', () => {
-  beforeAll(() => {
-    db.on('open', () => {
+describe('Blah blah', () => {
+  let nonExistentId: Schema.Types.ObjectId;
+
+  beforeAll( async () => {
+    db.on('open', async () => {
       console.log('Database starts successfully');
     });
+    const deletedTest: ITest = new Test({
+      name: 'This test will be deleted after I fetch id',
+      description: 'Whatever',
+      status: 'who cares',
+      runCmd: 'ls -la',
+    });
+    await deletedTest.save();
+    nonExistentId = deletedTest.id;
+    await deletedTest.remove();
   });
 
 
@@ -14,10 +28,14 @@ describe('GET / - a simple api endpoint', () => {
     return db.close();
   });
 
-  it('Hello API Request', async () => {
-    const result = await request(app).get('/all-runs');
-    expect(result.text).toEqual('hello');
-    expect(result.statusCode).toEqual(200);
+  it('asdasd', async () =>{
+    const response = await request(app)
+        .post('/add-run')
+        .send(
+            {agent: '2323', status: '3423', runCmd: 'ls -la', test: `${nonExistentId}`},
+        )
+        .set('Content-Type', 'application/json');
+    expect(response.text).toEqual('hello');
   });
 });
 
