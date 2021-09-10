dc334accdcfc3ab83cecb353b220fd8facfc91dc
Stepan Salin <ssalin@artec-group.com>
2021-09-10 18:32:24 +0300

    now able to add agents

--- src/controllers/agents/index.ts
+++ src/controllers/agents/index.ts
@@ -17,30 +17,28 @@ const addAgent = async (req: Request, res: Response): Promise<void> => {
     req.body as Pick<IAgent, 'name' | 'status'>;
 
     const query = await Agent.find({name: body.name});
-    console.log(query);
+    console.log(query.length);
+    if (query.length == 0) {
+      const agent: IAgent = new Agent({
+        name: body.name,
+        status: body.status,
+      });
 
-    // if (test == null) {
-    //   res
-    //       .status(404)
-    //       .json({message: `Test with id ${body.test} not found`});
-    // } else {
-    //   const run: IRun = new Run({
-    //     agent: body.agent,
-    //     status: body.status,
-    //     runCmd: body.runCmd,
-    //     test: body.test,
-    //   });
-
-      const newAgent: IAgent = await run.save();
-      const allRuns: IRun[] = await Run.find();
+      const newAgent: IAgent = await agent.save();
+      const allAgents: IAgent[] = await Agent.find();
 
       res
           .status(201)
-          .json({message: 'Run added', run: newRun, runs: allRuns});
+          .json({message: 'Agent added', run: newAgent, agents: allAgents});
+    } else {
+      res.
+          status(422)
+          .json({message: 'Agent already registered'});
     }
-    ;
   } catch (error) {
-    throw error;
+    res.
+        status(500)
+        .json({message: error});
   }
 };
-export {getRuns, addRun};
+export {getAgents, addAgent};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 import {getTests, addTest} from '../controllers/tests';
 import {getRuns, addRun} from '../controllers/runs';
+import {getAgents, addAgent} from '../controllers/agents';
 import bodyParser from 'body-parser';
 
 const jsonParser = bodyParser.json();
@@ -11,6 +12,8 @@ router.get('/all-tests', getTests);
 router.post('/add-test', jsonParser, addTest);
 router.get('/all-runs', getRuns);
 router.post('/add-run', jsonParser, addRun);
+router.post('/all-agents', jsonParser, getAgents);
+router.post('/add-agent', jsonParser, addAgent);
 
 
 export default router;
