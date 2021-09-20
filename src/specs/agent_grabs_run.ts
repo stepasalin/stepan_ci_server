d5bb5c50decd46edea4c96c06dc3322aff5f4eda
Stepan Salin <ssalin@artec-group.com>
2021-09-21 00:32:28 +0300

    continue doing agent grabs run spec

--- src/specs/agent_grabs_run.ts
+++ src/specs/agent_grabs_run.ts
@@ -1,21 +1,21 @@
-// import { app } from '../app';
+import { app } from '../app';
 import db from '../database/connection';
 import { Agent } from '../models/agent';
 import { IAgent } from '../types/agent';
-// import request from 'supertest';
+import request from 'supertest';
 // import { millisecondsSince } from '../util/milliseconds_since'
 import { IAutoTest } from '../types/auto_test';
 import AutoTest from '../models/auto_test'
 import { IRun } from '../types/run';
 import Run from '../models/run';
 
-// async function postToAddAgent(agentParams: Record<string, unknown>) {
-//   const response = await request(app)
-//     .post('/add-agent')
-//     .send(agentParams)
-//     .set('Content-Type', 'application/json');
-//   return response;
-// }
+async function postToGetRun(agentParams: Record<string, unknown>) {
+  const response = await request(app)
+    .post('/get-run')
+    .send(agentParams)
+    .set('Content-Type', 'application/json');
+  return response;
+}
 
 describe('Agent grabs Run', () => {
 
@@ -25,7 +25,7 @@ describe('Agent grabs Run', () => {
   let run1: IRun;
   let run2: IRun;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     db.on('open', async () => {
       console.log('Database starts successfully');
     });
@@ -35,16 +35,21 @@ describe('Agent grabs Run', () => {
 
     autoTest = new AutoTest({
       name: '',
-      description: 'for auto test, ideally you should not see this id DB',
+      description: 'for auto test, normally you should not see this id DB',
       runCmd: 'ls -la'
     });
     await autoTest.save();
 
     run1 = new Run({test: autoTest._id});
+    await run1.save();
     run2 = new Run({test: autoTest._id});
+    // run2 should not be saved just yet, we want to emulate the situation
+    // where run2 was created some time after run1 started
+    // "some time" could vary, bringing about
+    // a number of cases I'll cover in this spec
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     await agent.remove();
     await autoTest.remove();
     await run1.remove();
@@ -52,32 +57,11 @@ describe('Agent grabs Run', () => {
     return db.close();
   });
 
-  // it('can grab Run', async () => {
-  //   const agentCountBefore = await Agent.countDocuments();
-  //   const response = await postToAddAgent({name: agentCreatViaApiName})
+  it('can grab Run', async () => {
+    const response = await postToGetRun({agentId: agent._id})
 
-  //   expect(response.status).toEqual(201);
-  //   expect(response.body.message).toEqual('Agent added');
-  //   expect(response.body.agent.status).toEqual('free');
-  //   expect(response.body.agent.name).toEqual(agentCreatViaApiName);
-  //   const lastActiveAt = new Date(response.body.agent.lastActiveAt);
-  //   expect(millisecondsSince(lastActiveAt)).toBeLessThan(acceptableTimeInterval);
-  //   const newAgentId = response.body.agent._id
-  //   newAgent = await Agent.findById(newAgentId);
-  //   if (newAgent == null){
-  //     throw new Error(`Agent with id ${newAgentId} was not found in DB`);
-  //   }
-  //   const agentCountAfter = await Agent.countDocuments();
-  //   expect(agentCountAfter).toEqual(agentCountBefore + 1);
-  // });
-
-  // it('cannot register another agent with the same name', async () =>{
-  //   const agentCountBefore = await Agent.countDocuments();
-  //   const response = await postToAddAgent({name: agentAlreadyInDbName})
-  //   const agentCountAfter = await Agent.countDocuments();
-  //   expect(response.status).toEqual(422);
-  //   expect(response.body.message).toEqual('Agent already registered')
-  //   expect(agentCountAfter).toEqual(agentCountBefore)
-  // });
+    expect(response.status).toEqual(200);
+    expect(response.body.runId).toEqual(run1._id);
+  });
 })
 
